976717df4d6e6bf98b0f4921a75da34c69fd586d
Gabriel Ghiaroni <gabrielghiaroni@outlook.com>
2023-03-23 00:03:16 +0100

    comecando a renderizar em tela as receitas separadas por filtro

--- src/components/Categorias.jsx
+++ src/components/Categorias.jsx
@@ -7,22 +7,22 @@ import {NavLink} from 'react-router-dom';
 const Categorias = () => {
     const {italiana, americana, tailandesa, japonesa} = Filtros;
   return (
-    <div className='flex justify-center my-8'>
-        <NavLink to={'/cuisine/Italian'} className='flex flex-col items-center'>
-            <FaPizzaSlice />
-            <h4>{italiana}</h4>
+    <div className='flex justify-center my-8 gap-x-4'>
+        <NavLink to={'/cuisine/Italian'} className='flex flex-col items-center cursor-pointer'>
+            <div className='bg-sky-500 p-3 rounded-full'><FaPizzaSlice/></div>
+            <h4 className='font-medium text-sm pt-1'>{italiana}</h4>
         </NavLink>
-        <NavLink to={'/cuisine/American'} className='flex flex-col items-center'>
-            <FaHamburger />
-            <h4>{americana}</h4>
+        <NavLink to={'/cuisine/American'} className='flex flex-col items-center cursor-pointer'>
+            <div className='bg-sky-500 p-3 rounded-full'><FaHamburger /></div>
+            <h4 className='font-medium text-sm pt-1'>{americana}</h4>
         </NavLink>
-        <NavLink to={'/cuisine/Thai'} className='flex flex-col items-center'>
-            <GiNoodles />
-            <h4>{tailandesa}</h4>
+        <NavLink to={'/cuisine/Thai'} className='flex flex-col items-center cursor-pointer'>
+            <div className='bg-sky-500 p-3 rounded-full'><GiNoodles /></div>
+            <h4 className='font-medium text-sm pt-1'>{tailandesa}</h4>
         </NavLink>
-        <NavLink to={'/cuisine/Japanese'} className='flex flex-col items-center'>
-            <GiChopsticks />
-            <h4>{japonesa}</h4>
+        <NavLink to={'/cuisine/Japanese'} className='flex flex-col items-center cursor-pointer'>
+        <div className='bg-sky-500 p-3 rounded-full'><GiChopsticks /></div>
+            <h4 className='font-medium text-sm pt-1'>{japonesa}</h4>
         </NavLink>
     </div>
   )
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -8,8 +8,9 @@ const Cuisine = () => {
   let parametros = useParams();
 
   const getCuisine = async (tipo) => {
-    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${tipo}`);
+    const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&number=12&cuisine=${tipo}`);
     const receitas = await data.json();
+    console.log(receitas)
     setCuisine(receitas.results);
   };
 
@@ -18,7 +19,16 @@ const Cuisine = () => {
   }, [parametros.type]);
 
   return (
-    <div>Cuisine</div>
+    <div className='grid grid-cols-2 px-4 gap-y-8 gap-x-4 md:grid-cols-4'>
+      {cuisine.map((item, index) => {
+        return (
+          <div key={index}>
+            <img src={item.image} alt={item.title} className="w-full rounded-lg"/>
+            <h4 className='text-sm font-medium text-center pt-2'>{item.title}</h4>
+          </div>
+        )
+      })};
+    </div>
   )
 }
 
